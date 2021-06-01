b29f10e43bb3a6a44b9c32dd4253c1381ab99dee
Niv Sahar <71365529+nivsahar2@users.noreply.github.com>
2021-06-01 21:29:32 +0300

    Update AppCommands.cs

--- ConsoleApplication/AppCommands.cs
+++ ConsoleApplication/AppCommands.cs
@@ -1,12 +1,8 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-
-
-
 
 
 namespace CshConsoleAPI
@@ -15,10 +11,11 @@ namespace CshConsoleAPI
 	{
 		public const string CMD_PROMPED = "\n>";
 		public const string CMD_ECHO = "echo";
-
-
-
-
+		public const string CMD_EXIT = "exit";
+		public const string CMD_ADD = "add";
+		public const string CMD_SUB = "sub";
+		public const string CMD_MUL = "mul";
+		public const string CMD_DIV = "div";
 
 		/***
 		* Echo demonstrates a command execution function.
@@ -29,6 +26,59 @@ namespace CshConsoleAPI
 			foreach (string param in parameters){
 				Console.WriteLine(param);
 			}
+			return (true);
+		}
+
+		public static bool CommandExit(string[] parameters)
+		{
+			Console.WriteLine(CMD_EXIT);
+			Environment.Exit(0);
+			return (true);
+		}
+
+		public static bool CommandAdd(string[] parameters)
+		{
+			int sum = 0;
+			foreach (string param in parameters)
+			{
+				sum += Convert.ToInt32(param);
+			}
+			Console.WriteLine(sum.ToString());
+			return (true);
+		}
+
+		public static bool CommandSub(string[] parameters)
+		{
+			int sub = Convert.ToInt32(parameters[0]);
+
+			for (int i = 1; i < parameters.Length; i++)
+			{
+				sub -= Convert.ToInt32(parameters[i]);
+			}
+			Console.WriteLine(sub.ToString());
+			return (true);
+		}
+
+		public static bool CommandMul(string[] parameters)
+		{
+			int mul = 1;
+			foreach (string param in parameters)
+			{
+				mul *= Convert.ToInt32(param);
+			}
+			Console.WriteLine(mul.ToString());
+			return (true);
+		}
+
+		public static bool CommandDiv(string[] parameters)
+		{
+			double div = Convert.ToInt32(parameters[0]);
+				for (int i = 1; i < parameters.Length; i++)
+				{
+					if (Convert.ToInt32(parameters[i]) != 0)
+						div /= Convert.ToDouble(parameters[i]);
+				}
+				Console.WriteLine(div.ToString());
 			return (true);
 		}
 	}
